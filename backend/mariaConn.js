c86b70c1d0937c08b7ce88393294464bf2ffa785
김봉현 <85615745+BongHyun-Kim@users.noreply.github.com>
2022-06-10 15:55:33 +0900

    to secure

--- backend/mariaConn.js
+++ backend/mariaConn.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 //  Connection 객체 생성
 var connection = mysql.createConnection({
-  host: "3.38.180.156",
-  user: "user",
-  password: "1234",
-  database: "bi_solution",
+  host: "your ip_address",
+  user: "your user_id",
+  password: "your password",
+  database: "your database name",
 });
 
 // Connect
